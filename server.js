b3967380310ab6ca366fcae5b3404da02e902013
John Seyfert <johnseyfert@gmail.com>
2016-02-29 14:11:04 -0700

    added user auth

--- server.js
+++ server.js
@@ -1,19 +1,51 @@
+
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var Blog = require('./models/blog');
 var blogRouter = require('./routes/blogs');
 var mongoose = require('mongoose');
+
+var session = require('express-session');
+var flash = require('connect-flash');
+var passport = require('passport');
+
 var router = express.Router();
 var port = process.env.PORT || 8080;
 
+
+app.use(session({
+ secret: 'ilovescotchscotchyscotchscotch'
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(session({
+ cookie: {
+   maxAge: 60000
+ }
+}));
+app.use(flash());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(express.static('img'));
 app.use('/api', blogRouter);
+
+app.use(function(req, res, next){
+    var user = req.user || "no user";
+    console.log(user);
+    next();
+});
+
+require('./config/passport')(passport);
+// routes ======================================================================
+require('./routes/user.js')(app, passport);
+
+
 app.set('view engine', 'ejs');
 mongoose.connect('mongodb://localhost/blogs');
+
 
 router.use(function(req, res, next) { // middleware to 'use' for all requests
     console.log('Hell Yeah ' + port);
@@ -27,6 +59,7 @@ app.get('/', function(req, res){
 app.get('/photo', function(req, res){
     res.render('photo', {title: 'whats up!'});
 });
+
 
 app.get('/blog', function(req, res){
 
@@ -44,7 +77,3 @@ app.get('/blog', function(req, res){
 
 app.listen(port);
 console.log('There server is on PORT ' + port);
-
-// app.get('/about', function(req, res){
-//     res.render('about', {time: new Date()});
-// });
